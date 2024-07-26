1153572644d781c54bf6255cc2d930c7b5675e59
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-07-26 16:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+//your JS code here. If re
+const otp = document.querySelectorAll(".code");
+
+otp[0].focus();
+
+otp.forEach((code, idx) => {
+    otp.addEventListener("keydown", (e) => {
+        console.log(e.key);
+        const keyPressed = e.key;
+
+        if(keyPressed >=0 && keyPressed <= 9){
+            otp[idx].value = "";
+            if(idx < otp.length-1){
+                setTimeout(() => otp[idx+1].focus(), 10);
+            }
+        }else if(keyPressed === "BackSpace"){
+            if(idx > 0){
+                setTimeout(() => otp[idx-1].focus(), 10);
+            }
+        }
+    });
+});
